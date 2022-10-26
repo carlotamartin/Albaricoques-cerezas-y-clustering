eb70a4ef8f7a5622c4d42172eb78c7a0a6a2ba7b
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-10-26 10:49:16 +0200

    error

---
+++ codigo carlota/albaricoques.py
@@ -0,0 +1,36 @@
+import frutas
+import random
+
+class albaricoques ():
+    def __init__(self, lista) :
+        self.lista = lista
+
+
+    def __str__(self) :
+        return 'lista {}'.format(self.lista)
+
+    def generar_albaricoques(self, numero_observaciones):
+        albaricoques = []
+        random.seed()
+        for iteration in range(numero_observaciones):
+            #elección al azar de una característica
+            albaricoque = random.choice(self.lista)
+            #Generación de un diámetro
+            diametro = round(random.uniform(albaricoque[0], albaricoque[1]),2)
+            #Generación de un peso
+            limiteMinPeso = albaricoque[2] / 1.10
+            limiteMaxPeso = albaricoque[2] * 1.10
+            peso = round(random.uniform(limiteMinPeso, limiteMaxPeso),2)
+            print ("Albaricoque "+str(iteration)+" "+str(albaricoque)+" : "+str(diametro)+" - "+str(peso))
+            albaricoques.append([diametro,peso])
+        return albaricoques
+
+def main():
+    caracteristicasAlbaricoques = [[40,44,41],[45,49,54],[50,54,74],[55,59,100]]
+    cerezas = albaricoques(lista)
+    lista = cerezas.generar_albaricoques(500)
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ codigo carlota/frutas.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+import cerezas
+import albaricoques
+#tenemos que heredar los datos de cerezas y frutas que hemos generado para juntarlos
+
+class frutas ():
+    def __init__(self) :
+        pass
