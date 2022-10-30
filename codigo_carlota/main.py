6ec66747fbe9770cc3532151c0e8280784cdc056
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-10-30 20:25:01 +0100

    creación del main

--- codigo_carlota/main.py
+++ codigo_carlota/main.py
@@ -1,3 +1,9 @@
 import preparacion_de_datos.frutas as frutas
-import clustering.3D_curvas_gaussianas as 3D_curvas
 import clustering.cluster as cluster
+import clustering.3D_curvas_gaussianas as 3D_curvas_gaussianas
+
+
+if __name__ == '__main__':
+    frutas.main()
+    cluster.main()
+    3D_curvas_gaussianas.main()
